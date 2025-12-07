03056370dbc2af6aecc737363edb6e9e32e768cd
gelatinstudios <jellytoast1017@gmail.com>
2025-12-06 23:49:25 -0500

    day 5 part 2

--- 5.c
+++ 5.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define Min(a,b) ((a)<(b)?(a):(b))
+#define Max(a,b) ((a)>(b)?(a):(b))
+
 long long ranges[4096][2];
 int range_count = 0;
 
 int main(void) {
-    long long fresh_count;
     static char line[4096];
+    long long fresh_count;
+    int collapsing;
+    int i, j;
 
     while (fgets(line, sizeof(line), stdin)) {
         long long start, end;
@@ -24,7 +29,7 @@ int main(void) {
 
     fresh_count = 0;
     while (fgets(line, sizeof(line), stdin)) {
-        int fresh, i;
+        int fresh;
         long long n;
 
         if (isspace(line[0])) break;
@@ -39,6 +44,37 @@ int main(void) {
             }
         }
         fresh_count += fresh;
+    }
+
+    printf("%lld\n", fresh_count);
+
+    do {
+        collapsing = 0;
+        for (i = 0; i < range_count; i++) {
+            for (j = 0; j < range_count; j++) {
+                if (i==j) continue;
+                if ((ranges[j][0] <= ranges[i][0] && ranges[i][0] <= ranges[j][1]) ||
+                    (ranges[j][0] <= ranges[i][1] && ranges[i][1] <= ranges[j][1]))
+                {
+                    ranges[i][0] = Min(ranges[i][0], ranges[j][0]);
+                    ranges[i][1] = Max(ranges[i][1], ranges[j][1]);
+
+                    ranges[j][0] = ranges[range_count-1][0];
+                    ranges[j][1] = ranges[range_count-1][1];
+
+                    range_count--;
+
+                    collapsing = 1;
+                    break;
+                }
+            }
+            if (collapsing) break;
+        }
+    } while (collapsing);
+
+    fresh_count = 0;
+    for (i = 0; i < range_count; i++) {
+        fresh_count += ranges[i][1] - ranges[i][0] + 1;
     }
 
     printf("%lld\n", fresh_count);
