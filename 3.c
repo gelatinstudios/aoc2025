bf3c36ad0059d25ebb9eb1ec70aee3493caf156a
gelatinstudios <jellytoast1017@gmail.com>
2025-12-06 21:21:35 -0500

    day 3 part 2

--- 3.c
+++ 3.c
@@ -3,39 +3,51 @@
 #include <ctype.h>
 
 int main(void) {
-    int i;
+    long long joltages[2] = {0};
     static char line[1024];
-    int total_joltage = 0;
+    int part, i;
+
     while (fgets(line, sizeof(line), stdin)) {
-        char *c;
-        int a, a_index, b, joltage;
+        for (part = 0; part < 2; part++) {
+            char *c;
+            int last_index;
+            int digit, digits;
+            long long joltage, mul;
 
-        int len = 0;
-        for (c = line; isdigit(*c); c++) {
-            len++;
-        }
-
-        a = 0;
-        for (i = 0; i < len-1; i++) {
-            if (line[i] > a) {
-                a = line[i];
-                a_index = i;
+            int len = 0;
+            for (c = line; isdigit(*c); c++) {
+                len++;
             }
-        }
 
-        b = 0;
-        for (i = a_index+1; i < len; i++) {
-            if (line[i] > b) {
-                b = line[i];
+            if (part == 0) digits = 2;
+            else           digits = 12;
+
+            mul = 1;
+            for (i = 0; i < digits-1; i++) {
+                mul *= 10;
             }
+
+            joltage = 0;
+            last_index = -1;
+            for (digit = 0; digit < digits; digit++) {
+                int end = len - (digits - (digit+1));
+                int n = 0;
+                for (i = last_index+1; i < end; i++) {
+                    if (line[i] > n) {
+                        n = line[i];
+                        last_index = i;
+                    }
+                }
+                joltage += (long long)(n-'0') * mul;
+                mul /= 10;
+            }
+
+            joltages[part] += joltage;
         }
-
-        joltage = (a-'0')*10 + (b-'0');
-
-        total_joltage += joltage;
     }
 
-    printf("%d\n", total_joltage);
+    printf("%lld\n", joltages[0]);
+    printf("%lld\n", joltages[1]);
 
     return 0;
 }
