fe175cd833eaf21c12dbac57e44ec01f819aed39
gelatinstudios <jellytoast1017@gmail.com>
2025-12-07 17:11:32 -0500

    day 7 part 2

--- 7.c
+++ 7.c
@@ -3,22 +3,33 @@
 #include <assert.h>
 
 typedef struct {
-    int beams[4096];
+    int pos;
+    long long timelines;
+} Beam;
+
+typedef struct {
+    Beam beams[4096];
     int count;
 } Beams;
 
-void add_beam(Beams *b, int n) {
+void add_beam(Beams *b, int pos, long long timelines) {
     int i;
     for (i = 0; i < b->count; i++) {
-        if (b->beams[i] == n) return;
+        if (b->beams[i].pos == pos) {
+            b->beams[i].timelines += timelines;
+            return;
+        }
     }
-    b->beams[b->count++] = n;
+    b->beams[b->count].pos = pos;
+    b->beams[b->count].timelines = timelines;
+    b->count++;
 }
 
 int main(void) {
     static char line[4096];
     Beams beams = {0};
     int split_count;
+    long long timelines;
     int i;
     char *c;
 
@@ -26,7 +37,7 @@ int main(void) {
 
     for (c = line; *c; c++) {
         if (*c == 'S') {
-            beams.beams[beams.count++] = c - line;
+            add_beam(&beams, c - line, 1);
         }
     }
 
@@ -34,19 +45,28 @@ int main(void) {
     while (fgets(line, sizeof(line), stdin)) {
         Beams b = {0};
         for (i = 0; i < beams.count; i++) {
-            int n = beams.beams[i];
+            Beam beam = beams.beams[i];
+            int n = beam.pos;
+            long long t = beam.timelines;
             if (line[n] == '^') {
-                add_beam(&b, n-1);
-                add_beam(&b, n+1);
+                add_beam(&b, n-1, t);
+                add_beam(&b, n+1, t);
                 split_count++;
             } else {
-                add_beam(&b, n);
+                add_beam(&b, n, t);
             }
         }
         beams = b;
     }
 
     printf("%d\n", split_count);
+
+    timelines = 0;
+    for (i = 0; i < beams.count; i++) {
+        timelines += beams.beams[i].timelines;
+    }
+
+    printf("%lld\n", timelines);
 
     return 0;
 }
