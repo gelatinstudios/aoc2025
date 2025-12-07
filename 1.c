35d54cc058757858a0d86d723df2561431f1bce5
gelatinstudios <jellytoast1017@gmail.com>
2025-12-06 19:03:56 -0500

    day 1 part 2

--- 1.c
+++ 1.c
@@ -1,20 +1,30 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
     int dial = 50;
-    int zero_count = 0;
+    int zero_count1 = 0;
+    int zero_count2 = 0;
 
-    static char line[4096];
-    while (fgets(line, sizeof(line), stdin)) {
-        char dir;
-        int n;
-        sscanf(line, "%c%d", &dir, &n);
-        if (dir == 'L') n = -n;
-        dial = (dial + n + 100) % 100;
-        zero_count += (dial == 0);
+    char dir;
+    int n;
+    while (scanf("%c%d ", &dir, &n) > 0) {
+        int m = n % 100;
+        if (dial != 0) {
+            if (dir=='R') {
+                zero_count2 += (dial + m >= 100);
+            } else {
+                zero_count2 += (dial - m <= 0);
+            }
+        }
+        zero_count2 += n/100;
+        if (dir == 'L') m = -m;
+        dial = (dial + m + 100) % 100;
+        zero_count1 += (dial==0);
     }
-    printf("%d\n", zero_count);
+    printf("%d\n", zero_count1);
+    printf("%d\n", zero_count2);
 
     return 0;
 }
