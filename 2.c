36bc5818026860497063326e839dbabf948a77a5
gelatinstudios <jellytoast1017@gmail.com>
2025-12-06 20:37:06 -0500

    day 2 part 2

--- 2.c
+++ 2.c
@@ -7,6 +7,7 @@ int main(void) {
     char *range;
     int i;
     long long part1 = 0;
+    long long part2 = 0;
     static char input[256*1024];
     fgets(input, sizeof(input), stdin);
 
@@ -16,29 +17,44 @@ int main(void) {
         sscanf(range, "%lld-%lld", &start, &end);
 
         for (id = start; id <= end; id++) {
+            long long a, m;
+            int part, id_valid;
             long long n = id;
-            long long a, b, m;
             int digit_count = 0;
             while (n > 0) {
                 digit_count++;
                 n /= 10;
             }
-            if (digit_count % 2 != 0) continue;
-            m = 1;
-            for (i = 0; i < digit_count/2; i++)
-                m *= 10;
-            a = id / m;
-            b = id % m;
-            if (a == b) {
-                part1 += id;
+            id_valid = 1;
+            for (part = 2; part < 8; part++) {
+                int part_valid = 0;
+                if (digit_count % part != 0) continue;
+                m = 1;
+                for (i = 0; i < digit_count/part; i++)
+                    m *= 10;
+                a = id % m;
+                n = id;
+                for (i = 0; i < part; i++) {
+                    if (n % m != a) {
+                        part_valid = 1;
+                        break;
+                    }
+                    n /= m;
+                }
+                if (!part_valid) {
+                    if (part == 2) part1 += id;
+                    id_valid = 0;
+                    break;
+                }
             }
+            if (!id_valid) part2 += id;
         }
 
         range = strtok(0, delims);
     }
 
     printf("%lld\n", part1);
-
+    printf("%lld\n", part2);
 
     return 0;
 }
